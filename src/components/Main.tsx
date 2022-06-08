86c91f7ceb92d06935cb4ef37c4e264a87ef43fc
karanzhenya <63317606+karanzhenya@users.noreply.github.com>
2022-06-08 12:56:59 +0300

    added sorting data

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -42,12 +42,14 @@ export const Main = ({setActive, setInfo}: MainPropsType) => {
     if (allCars.length === 0) {
         return <Preloader/>
     }
+    const sortingCars = allCars.sort((a, b) => a.name > b.name ? 1 : -1)
+    const sortingCarSpecs = carSpecs.sort((a, b) => a.type > b.type ? 1 : -1)
     return (
         <div>
             <MyInput placeholder={"Поиск по модели"} error={error} onChangeText={debouncedChangeHandler}
                      className={s.input}/>
-            <Cars filterCars={allCars} openCarType={openCarType}/>
-            <CarTypeList openModelInfo={openModelInfo} carSpecs={carSpecs}/>
+            <Cars filterCars={sortingCars} openCarType={openCarType}/>
+            <CarTypeList openModelInfo={openModelInfo} carSpecs={sortingCarSpecs}/>
         </div>
     );
 };
